2832c4d05f8ce7986dde40d6bfae6a669ccf0a05
Alex Storm <info@solenopsys.org>
2025-03-20 18:39:38 +0700

    export

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,4 @@
 pub usingnamespace @import("./pack-builder.zig");
 pub usingnamespace @import("./pack-parser.zig");
 pub usingnamespace @import("./types.zig");
+pub usingnamespace @import("./routing.zig");
